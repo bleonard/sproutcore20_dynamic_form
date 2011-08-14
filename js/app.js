b1b58f2b3c84750bc3f4a8c8c897609dec801e41
Brian Leonard <brian@bleonard.com>
2011-08-13 17:27:14 -0700

    learned how to list out task types

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 var App = SC.Application.create();
 
-App.MyView = SC.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+App.TaskType = SC.Object.extend({
+  name: null
+});
+
+App.taskTypesController = SC.ArrayProxy.create({
+  // Initialize the array controller with the types
+  content: [App.TaskType.create({ name: "Shopping" }), App.TaskType.create({ name: "IKEA" })]
+});
+
+App.TaskTypesCollectionView = SC.CollectionView.extend({
+  contentBinding: 'App.taskTypesController',
+	tagName: 'ul'
+});
+
+
+App.TaskTypeView = SC.View.extend({
+	name: null
 });
